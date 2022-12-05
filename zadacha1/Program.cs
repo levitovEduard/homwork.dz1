918f0e504b2d22d464bd3482e0f009e2ed696eba
Eduard <levitov.eduard@gmail.com>
2022-12-05 22:30:25 +0200

    Определили какое число больше, а какое меньше

---
+++ zadacha1/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1 =10;
+int num2 = 12;
+         
+         
+         if (num1 > num2)
+            
+                Console.WriteLine("Max {0}, min {1}", num1, num2);
+            
+         else    
+                Console.WriteLine("Max {0}, min {1}", num2, num1);
+
