bf21b1ab0eed81945289135eb540c3b6175ee691
Eduard <levitov.eduard@gmail.com>
2022-12-06 12:51:42 +0200

    Задача на три числа

---
+++ zadacha2/Program.cs
@@ -0,0 +1,14 @@
+﻿int num1 =7, num2 = 97, num3= 71, max; 
+   
+
+        max = num1;
+
+        if(max < num2)
+        {
+            max = num2;
+        }
+        if(max < num3)
+        {
+            max = num3;
+        }
+        Console.WriteLine(max);
