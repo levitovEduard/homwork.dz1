37957eb4203a3b775f5fc2573a8d0242e9cabeb1
Eduard <levitov.eduard@gmail.com>
2022-12-07 22:14:21 +0200

    Zadacha 4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу которая принимает на вход число (N), а на выходе показывает все четные числа от 1 до N.
+{  
+    {
+         int num;
+         int ind =2;
+         
+         num = Convert.ToInt32(Console.ReadLine());
+
+         while (ind <= num)
+    {
+         Console.WriteLine (ind);
+         ind= ind+2;
+    }
+        
+    }
+}
